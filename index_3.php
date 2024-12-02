fc6dc49e21beb8f74676eaeac9b8a1c0662020b5
Rafail Abdyushev <rafaelab@mail.ru>
2024-12-02 10:58:44 +0300

    index_3.php

--- index_3.php
+++ index_3.php
@@ -272,8 +272,45 @@ option:hover,  .active{
 </div>
 
 <script type="text/javascript">
-    $(document).ready(function () {
-        const parseCurrencies = (result) => {
+    
+    function CurrensyData() {
+            ymaps.ready(init);
+            let self = this;
+            this.dataFromApi = [];
+            
+            function init() {
+                var geolocation = ymaps.geolocation;
+
+                geolocation.get({
+                    provider: 'yandex',
+                    mapStateAutoApply: false,
+                }).then(function(result) {
+                    // result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+                    result.geoObjects.options.set('iconImageHref', '/img/icons/pin-1.png');
+                                    result.geoObjects.options.set('iconImageSize', [40, 48]);
+                    result.geoObjects.get(0).properties.set({
+                        balloonContentBody: 'Мое местоположение'
+                    });
+                    myMap.geoObjects.add(result.geoObjects);
+                    console.log(result.geoObjects.position);
+                    self.requestToApi(result.geoObjects.position);
+                });
+
+
+                var myMap = new ymaps.Map("map", { 
+                                            center: [55.741206, 37.614267], 
+                                            zoom: 12 
+                            });
+                            //           myMap.controls.add("zoomControl").add("typeSelector");
+                            //var myPlacemark = new ymaps.Placemark([55.741206, 37.614267], {
+                            //	});
+            }
+            
+            this.initSettings();
+    }
+    
+    CurrensyData.prototype.parseCurrencies = function(result) {
+//        const parseCurrencies = (result) => {
             let parentCurrency  = document.querySelectorAll('.currency-list-block')[0];
             let childCurrency = document.getElementById("example-curr");
             result.forEach((el, ind) => {
@@ -288,7 +325,8 @@ option:hover,  .active{
             });
         }
         
-        const requestToApi = async (position) => {
+        CurrensyData.prototype.requestToApi = async function(position) {
+        //const requestToApi = async (position) => {
 //            let url = new URL("./rus/private/currtest2/db.php");
             let checkPosition = [parseFloat(position[0]), parseFloat(position[1])]; 
             let url = "./apidb.php";
@@ -306,105 +344,100 @@ option:hover,  .active{
                     throw new Error(`Response status: ${response.status}`);
                 }
                 const data = await response.json();
-                let closestOffice = data.data.reduce(function(prev, curr) {
+                this.dataFromApi = data.data;
+                let closestOffice = this.dataFromApi.reduce(function(prev, curr) {
                     let data1 = Math.sqrt( Math.pow((parseFloat(prev.latitude) - checkPosition[0]) ,2) + Math.pow((parseFloat(prev.longitude) - checkPosition[1]),2) );
                     let data2 = Math.sqrt( Math.pow((parseFloat(curr.latitude) - checkPosition[0]),2) + Math.pow((parseFloat(curr.longitude) - checkPosition[0]),2) );
-                   
                     return (Math.abs(data2) < Math.abs(data1) ? curr : prev);
                 });
-                console.log("from api result = ", checkPosition);
-                console.log("from api result = ", closestOffice);
-                let result = data.data.filter(x => x.office_id == closestOffice.office_id);
+                console.log("from api result ближайший офис = ", checkPosition);
+                console.log("список валют = ", closestOffice);
+                let result = this.dataFromApi.filter(x => x.office_id == closestOffice.office_id);
                 console.log("from api result = ", result);
-                parseCurrencies(result);
+                console.log("from api result город = ", result);
+                this.parseCurrencies(result);
+                this.changeCitiesOffices(closestOffice);
             }
             catch(error) {
                 console.log(error.message);
             }
             console.log("hello yandex");
         } 
+    
+        CurrensyData.prototype.changeCitiesOffices = function(closestOffice) {
+            let currentOfficesInTheCity = this.dataFromApi.filter(x => x.city == closestOffice.city);
+            let listOfCities = [...new Set(Array.from(this.dataFromApi, x => x.city))];
+            console.log("Кол-во офисов в горое = ", listOfCities.length, listOfCities[20]);
+            let citiesFieldLink = document.querySelectorAll(".db")[0];
+            let citiesDataLink = document.querySelectorAll(".db-datalist")[0];
+            let options = citiesDataLink.children;
+            var i, L = citiesDataLink.options.length - 1;
+            for(i = L; i >= 0; i--) {
+               citiesDataLink.remove(i);
+            }
+        }
         
-        ymaps.ready(init);
-        function init() {
-            var geolocation = ymaps.geolocation;
+    
+        CurrensyData.prototype.initSettings = function() {
+            let fieldLink = document.querySelectorAll(".db");
+            let dataLink = document.querySelectorAll(".db-datalist");
+            let dataAllLinks = [];
             
-            geolocation.get({
-                provider: 'yandex',
-                mapStateAutoApply: false,
-            }).then(function(result) {
-                // result.geoObjects.options.set('preset', 'islands#redCircleIcon');
-                result.geoObjects.options.set('iconImageHref', '/img/icons/pin-1.png');
-				result.geoObjects.options.set('iconImageSize', [40, 48]);
-                result.geoObjects.get(0).properties.set({
-                    balloonContentBody: 'Мое местоположение'
-                });
-                myMap.geoObjects.add(result.geoObjects);
-                console.log(result.geoObjects.position);
-                requestToApi(result.geoObjects.position);
+            dataLink.forEach((el, index) => {
+                dataAllLinks.push(Array.from(el.options, (option) => option.value));
             });
 
-            
-            var myMap = new ymaps.Map("map", { 
-                                        center: [55.741206, 37.614267], 
-                                        zoom: 12 
-                        });
-                        //           myMap.controls.add("zoomControl").add("typeSelector");
-                        //var myPlacemark = new ymaps.Placemark([55.741206, 37.614267], {
-                        //	});
-        }
-
-        let fieldLink = document.querySelectorAll(".db");
-        let dataLink = document.querySelectorAll(".db-datalist");
-        let dataAllLinks = [];
-        dataLink.forEach((el, index) => {
-            dataAllLinks.push(Array.from(el.options, (option) => option.value));
-        });
-
-        fieldLink.forEach((el, index) => {
-            el.onfocus = function () {
-                dataLink[index].style.display = 'block';
-                el.style.borderRadius = "5px 5px 0 0";  
-            };
-
-            for (let option of dataLink[index].options) {
-                option.onmousedown = function () {
-                    event.preventDefault();
-                }
-                option.onclick = function () {
-                    el.value = option.value;
-                    dataLink[index].style.display = 'none';
-                    el.style.borderRadius = "5px";
-                    let anotherInput = index & 1 == 1 ? index - 1 : index + 1;
-                    console.log("Check tab - ", fieldLink.length, index, anotherInput);
-                    let valueForNotherInput = dataAllLinks[anotherInput][dataAllLinks[index].indexOf(option.value)];
-                    fieldLink[anotherInput].value = valueForNotherInput;
-                    console.log("Hello - ", dataAllLinks[index].indexOf(option.value));
-                    el.blur();
-                }
-            }
-
-            el.oninput = function() {
-                let currentFocus = -1;
-                let text = el.value.toUpperCase();
-                for (let option of dataLink[index].options) {
-                    if(option.value.toUpperCase().indexOf(text) > -1) {
-                        option.style.display = "block";
-                    } else {
-                        option.style.display = "none";
-                    }
+            fieldLink.forEach((el, index) => {
+                el.onfocus = function () {
+                    dataLink[index].style.display = 'block';
+                    el.style.borderRadius = "6px 6px 0 0";  
                 };
-            }
 
-        });
+                for (let option of dataLink[index].options) {
+                    option.onmousedown = function () {
+                        event.preventDefault();
+                    }
+                    option.onclick = function () {
+                        el.value = option.value;
+                        dataLink[index].style.display = 'none';
+                        el.style.borderRadius = "6px";
+                        let anotherInput = index & 1 == 1 ? index - 1 : index + 1;
+                        console.log("Check tab - ", fieldLink.length, index, anotherInput);
+                        let valueForNotherInput = dataAllLinks[anotherInput][dataAllLinks[index].indexOf(option.value)];
+                        fieldLink[anotherInput].value = valueForNotherInput;
+                        console.log("Hello - ", dataAllLinks[index].indexOf(option.value));
+                        el.blur();
+                    }
+                }
 
-        fieldLink.forEach((el, index) => {
-            el.onblur = function (event) {
-                dataLink[index].style.display = 'none';
-                el.style.borderRadius = "0px";  
+                el.oninput = function() {
+                    let currentFocus = -1;
+                    let text = el.value.toUpperCase();
+                    for (let option of dataLink[index].options) {
+                        if(option.value.toUpperCase().indexOf(text) > -1) {
+                            option.style.display = "block";
+                        } else {
+                            option.style.display = "none";
+                        }
+                    };
+                }
 
-            };
-        });
+            });
+
+            fieldLink.forEach((el, index) => {
+                el.onblur = function (event) {
+                    dataLink[index].style.display = 'none';
+                    el.style.borderRadius = "6px 6px 6px 6px";  
+
+                };
+            });
+        }
+    
+    
+    $(document).ready(function () {
+        let obj = new CurrensyData();
     });
+    
 </script>
 <script type="text/javascript">
     $(document).ready(function () {
@@ -425,7 +458,7 @@ option:hover,  .active{
 </script>
 <script type="text/javascript">
 </script>
-
+<?require_once(getcwd() . "/jsmodule.php")?>
 
 
     
